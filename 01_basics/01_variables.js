0c7d8e9cb0cce9fa21545f3f381e3ef49d8e29c0
Ayan-B <ayan.biswas2023@vitstudent.ac.in>
2025-08-14 18:57:35 +0000

    Add variable declarations and data type examples in JavaScript files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "user@example.com"
+var accountPassword = "securePassword123"
+accountCity = "jaipur"
+
+
+accountEmail = "hc@example.com"
+accountPassword = "newSecurePassword456"
+accountCity = "bengaluru"
+
+console.table([ accountId, accountEmail, accountPassword, accountCity  ])
